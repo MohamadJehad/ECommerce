ac4b588bf6a9cbd89d7e5ea8bd4b54c4c36062f9
MohamadJehad <mohamad.jehad@remotecoders.org>
2025-04-26 14:47:36 +0300

    add `PhotoRepository`

--- ECommerce.Infrastructure/InfrastructureRegisteration.cs
+++ ECommerce.Infrastructure/InfrastructureRegisteration.cs
@@ -16,6 +16,7 @@ namespace ECommerce.Infrastructure
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IPhotoRepository, PhotoRepository>();
 
             return services;
         }
