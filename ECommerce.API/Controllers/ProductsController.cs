1880d46028c953428538879a02c0102906402f0f
MohamadJehad <mohamad.jehad@remotecoders.org>
2025-04-28 22:30:52 +0300

    adjust product repository to delete endpoint to delete categories and photos related to product

--- ECommerce.API/Controllers/ProductsController.cs
+++ ECommerce.API/Controllers/ProductsController.cs
@@ -86,7 +86,9 @@ namespace ECommerce.API.Controllers
         {
             try
             {
-                await work.ProductRepository.DeleteAsync(id);
+                var product = await work.ProductRepository.GetByIdAsync(id, x=> x.Photos, x=>x.Category);
+
+                await work.ProductRepository.DeleteAsync(product);
                 return Ok(new ResponseAPI(200, "Item has been Deleted"));
             }
             catch (Exception ex)
