94c5b3537655551a56e22ed516793bc6e3e68452
MohamadJehad <mohamad.jehad@remotecoders.org>
2025-04-29 23:47:43 +0300

    add middleware file

---
+++ ECommerce.API/Middleware/ExceptionMeddleware.cs
@@ -0,0 +1,24 @@
+﻿namespace ECommerce.API.Middleware
+{
+    public class ExceptionMeddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMeddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task TaskAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
