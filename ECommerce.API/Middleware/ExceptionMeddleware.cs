3b219a510a139d378c537f68abae0cbdc8926f9b
MohamadJehad <mohamad.jehad@remotecoders.org>
2025-04-30 00:01:12 +0300

    add function to handle trace limit

--- ECommerce.API/Middleware/ExceptionMeddleware.cs
+++ ECommerce.API/Middleware/ExceptionMeddleware.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerce.API.Helper;
+using Microsoft.Extensions.Caching.Memory;
 using System.Net;
 using System.Text.Json;
 
@@ -6,18 +7,23 @@ namespace ECommerce.API.Middleware
 {
     public class ExceptionMeddleware
     {
-        private readonly RequestDelegate _next;
+        private readonly RequestDelegate next;
+        private readonly IHostEnvironment environment;
+        private readonly IMemoryCache memoryCache;
+        private readonly TimeSpan rateLimitWindow = TimeSpan.FromSeconds(30);
 
-        public ExceptionMeddleware(RequestDelegate next)
+        public ExceptionMeddleware(RequestDelegate next, IHostEnvironment environment, IMemoryCache memoryCache)
         {
-            _next = next;
+            this.next = next;
+            this.environment = environment;
+            this.memoryCache = memoryCache;
         }
 
         public async Task TaskAsync(HttpContext context)
         {
             try
             {
-                await _next(context);
+                await next(context);
             }
             catch (Exception ex)
             {
@@ -28,6 +34,35 @@ namespace ECommerce.API.Middleware
                 var json = JsonSerializer.Serialize(response);
                 await context.Response.WriteAsync(json);
             }
+        }
+
+        private bool IsRequestAllowed (HttpContext context)
+        {
+            var ip = context.Connection.RemoteIpAddress.ToString();
+            var cacheKey = $"Rate:{ip}";
+            var dateNow = DateTime.Now;
+
+            var (timesTamp, count) = memoryCache.GetOrCreate(cacheKey, entry =>
+                {
+                    entry.AbsoluteExpirationRelativeToNow = rateLimitWindow;
+                    return (timesTamp: dateNow, count: 0);
+                });
+
+            if (dateNow - timesTamp < rateLimitWindow)
+            {
+                if (count >= 8)
+                {
+                    return false;
+                }
+
+                memoryCache.Set(cacheKey, (timesTamp, count += 1), rateLimitWindow);
+            }
+            else
+            {
+                memoryCache.Set(cacheKey, (timesTamp, count), rateLimitWindow);
+            }
+
+            return true;
         }
     }
 }
